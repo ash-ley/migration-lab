428577c19f6209c27a507d88b5a8da528fe52342
ash-ley <ashleyritchie1871@hotmail.co.uk>
2023-06-06 15:48:30 +0100

    remove commented code

--- migration/vpc.tf
+++ migration/vpc.tf
@@ -1,10 +1,3 @@
-/* locals {
-  availability-zones = slice(data.aws_availability_zones.available.names, 0, var.number-of-azs)
-
-  public_subnet_cidr  = [cidrsubnet(var.vpc-cidr, 8, 0), cidrsubnet(var.vpc-cidr, 8, 1)]
-  private_subnet_cidr = [cidrsubnet(var.vpc-cidr, 8, 2), cidrsubnet(var.vpc-cidr, 8, 3)]
-} */
-
 locals {
   availability-zones      = slice(data.aws_availability_zones.available.names, 0, var.number-of-azs)
 
